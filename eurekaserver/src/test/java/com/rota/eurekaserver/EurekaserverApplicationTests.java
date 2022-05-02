cc36624fad3428f2acd4134bf450148ad3e15caa
hp <hp@DESKTOP-J63SKVU>
2022-05-02 08:01:46 +0530

    Eureka server initial commit

---
+++ eurekaserver/src/test/java/com/rota/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rota.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
